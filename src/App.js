3a6b3d72d0c9d37c786db5da5144346b6535d208
Sean McDonald <sean.mcdonald@connected-space.co.uk>
2018-01-24 11:57:16 +0000

    finished app. don't know how to commit from terminal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,62 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { View } from 'react-native'
+import firebase from 'firebase'
+import {
+  Header,
+  Button,
+  Spinner,
+  CardSection,
+  StatusBarBackground
+} from './components/common'
+import LoginForm from './components/LoginForm'
 
 class App extends Component {
+  state = { loggedIn: null }
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyD0krU_qUTtT8fxv3fqFs6BOZkz7iwso7I',
+      authDomain: 'auth-3f9f8.firebaseapp.com',
+      databaseURL: 'https://auth-3f9f8.firebaseio.com',
+      projectId: 'auth-3f9f8',
+      storageBucket: 'auth-3f9f8.appspot.com',
+      messagingSenderId: '1001661237199'
+    })
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true })
+      } else {
+        this.setState({ loggedIn: false })
+      }
+    })
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <CardSection>
+            <Button onPress={() => firebase.auth().signOut()}>Log out</Button>
+          </CardSection>
+        )
+      case false:
+        return <LoginForm />
+      default:
+        return (
+          <CardSection>
+            <Spinner />
+          </CardSection>
+        )
+    }
+  }
+
   render() {
     return (
       <View>
-        <Text>An App!</Text>
+        <StatusBarBackground style={{ backgroundColor: '#007aff' }} />
+        <Header headerText="Authentication" />
+        {this.renderContent()}
       </View>
     )
   }
